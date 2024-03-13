5dc511241c984fe382b467a2a50e12e979ea679d
ElbekRahmanov@yandex.ru <ElbekRahmanov@yandex.ru>
2024-03-13 15:11:46 +0200

    change url 2 try

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -4,7 +4,7 @@ import Tracklist from "./Tracklist";
 const SearchResults = ({ filteredData, onAdd }) => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.h1}>Results: </h1>
+      <h1 className={styles.h1}>Rets: </h1>
       <Tracklist filteredData={filteredData} onAdd={onAdd} />
     </div>
   );
